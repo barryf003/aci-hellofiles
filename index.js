1805f5a9d2e6878a54076855398938015d42f439
Barry Fitz <barryf003@users.noreply.github.com>
2023-11-04 21:16:42 +0000

    test

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post('/values', (req, res) => {
-  console.log(req.body)
+app.post("/values", (req, res) => {
+  console.log(req.body);
 
   var filename = "/aci/logs/" + Date.now() + ".txt";
+  var filename2 = "/aci/logs2/" + Date.now() + ".txt";
 
-  fs.writeFile(filename, req.body.name, function(err) {
-    if(err) {
+  fs.writeFile(filename, req.body.name, function (err) {
+    if (err) {
       return console.log(err);
     }
-    
+
     console.log("Wrote to file");
   });
 
-  res.redirect('/');
-})
+  fs.writeFile(filename2, req.body.name, function (err) {
+    if (err) {
+      return console.log(err);
+    }
 
-app.listen(process.env.PORT || 80, function() {
- console.log('listening on port ' + process.env.PORT || 80);
+    console.log("Wrote to file - 02");
+  });
+
+  res.redirect("/");
 });
 
+app.listen(process.env.PORT || 80, function () {
+  console.log("listening on port " + process.env.PORT || 80);
+});
